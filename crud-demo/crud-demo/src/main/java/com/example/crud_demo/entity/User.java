c654550b15ea1c4a4e1f551eebf161b3609b6341
Priyanka2546 <88018067+Priyanka2546@users.noreply.github.com>
2025-05-10 12:52:08 +0530

    Add files via upload
    Simple crude operation using springboot.

---
+++ crud-demo/crud-demo/src/main/java/com/example/crud_demo/entity/User.java
@@ -0,0 +1,48 @@
+package com.example.crud_demo.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+// Getters & Setters
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+
+    }
+}
---
+++ crud-demo/crud-demo/src/main/java/com/example/crud_demo/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.example.crud_demo.repository;
+
+import com.example.crud_demo.entity.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
